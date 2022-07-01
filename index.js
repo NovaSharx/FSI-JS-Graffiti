649fbec4c7910c22cc1ceea30b1b17affe8a5dd1
Davin <davin-dj@hotmail.com>
2022-07-01 14:09:12 -0400

    Completed all the bonus challenges.

--- index.js
+++ index.js
@@ -4,7 +4,14 @@
 let header = document.querySelector('#page-header')
 header.style.textAlign = 'left'
 
+let dogList = document.querySelectorAll('.column')
 let dogImages = document.querySelectorAll('.dog-image')
-for (let i = 0; i < dogImages.length; i++) {
+let dogNames = document.querySelectorAll('.dog-name')
+for (let i = 0; i < dogList.length; i++) {
     dogImages[i].style.borderRadius = '50px'
+    dogImages[i].style.transform = 'rotate(180deg)'
+    dogNames[i].style.textAlign = 'left'
 }
+
+let footer = document.querySelector('.footer')
+footer.style.color = 'blue'
